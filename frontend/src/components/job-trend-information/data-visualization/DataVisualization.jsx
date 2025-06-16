80b3132f5cc534d5c52cc594f3dbb9f17f8c5911
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-06-16 15:54:37 +0700

    [UPDATE] - Components - Data Visualization

--- frontend/src/components/job-trend-information/data-visualization/DataVisualization.jsx
+++ frontend/src/components/job-trend-information/data-visualization/DataVisualization.jsx
@@ -5,24 +5,33 @@ import GlintsJobWorkTypeCard from "./glints/GlintsJobWorkTypeCard.jsx";
 import JobstreetJobSubCategoryCard from "./jobstreet/JobstreetJobSubCategoryCard.jsx";
 import GlintsJobSubCategoryCard from "./glints/GlintsJobSubCategoryCard.jsx";
 import JobstreetJobLocationCard from "./jobstreet/JobstreetJobLocationCard.jsx";
-import GlintsJobLocationCard from "./glints/GlintJobLocationCard.jsx";
+import GlintsJobLocationCard from "./glints/GlintsJobLocationCard.jsx";
 import JobstreetJobSalaryCard from "./jobstreet/JobstreetJobSalaryCard.jsx";
 import GlintsJobSalaryCard from "./glints/GlintsJobSalaryCard.jsx";
+import jobstreetsLogo from "../../../assets/Jobstreets.png";
+import glintsLogo from "../../../assets/Glints.png";
 
 const DataVisualization = () => {
   return (
     <main className="p-6 mt-3">
       <div className="grid grid-cols-1 space-y-4">
+        <div className="border rounded-lg p-4 bg-blue-50 ">
+          <img src={jobstreetsLogo} className="h-12" />
+        </div>
         <JobstreetJobCategoryCard />
-        <GlintsJobCategoryCard />
         <JobstreetJobSubCategoryCard />
-        <GlintsJobSubCategoryCard />
-        <JobstreetJobWorkTypeCard />
-        <GlintsJobWorkTypeCard />
-        <JobstreetJobLocationCard />  
-        <GlintsJobLocationCard />   
+        <JobstreetJobLocationCard />
         <JobstreetJobSalaryCard />
-        <GlintsJobSalaryCard />   
+        <JobstreetJobWorkTypeCard />
+      
+        <div className="border rounded-lg p-4 bg-orange-50 ">
+          <img src={glintsLogo} className="h-16" />
+        </div>
+        <GlintsJobCategoryCard />
+        <GlintsJobSubCategoryCard />
+        <GlintsJobLocationCard />
+        <GlintsJobSalaryCard />
+        <GlintsJobWorkTypeCard />
       </div>
     </main>
   );
