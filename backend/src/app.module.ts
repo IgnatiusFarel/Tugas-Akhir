518b4e15e39090ffb9a2aaf0f3141206a7582583
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-07-03 23:25:45 +0700

    [UPDATE] - App Module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,8 +3,9 @@ import { PrismaModule } from './prisma/prisma.module';
 import { ExtractDataModule } from './job-trend-information/extract-data/extract-data.module';
 import { DataCollectionModule } from './job-trend-information/data-collection/data-collection.module';
 import { DataVisualizationModule } from './job-trend-information/data-visualization/data-visualization.module';
+import { ScrapeConfigModule } from './job-trend-information/scrape-config/scrape-config.module';
 
 @Module({
-  imports: [ExtractDataModule, DataCollectionModule, DataVisualizationModule, PrismaModule], 
+  imports: [ExtractDataModule, DataCollectionModule, DataVisualizationModule, ScrapeConfigModule, PrismaModule], 
 })
 export class AppModule {}
