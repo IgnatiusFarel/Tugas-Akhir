4d0ca2c41075574cd7ffb090611cd304de12fd9e
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-07-03 23:36:48 +0700

    [UPDATE] - Routes - Index

--- frontend/src/routes/Index.jsx
+++ frontend/src/routes/Index.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import ExtractDataPage from "../pages/job-trend-information/ExtractDataPage";
 import DataCollectionPage from "../pages/job-trend-information/DataCollectionPage";
 import DataVisualizationPage from "../pages/job-trend-information/DataVisualizationPage";
+import ScrapeConfigPage from "../pages/job-trend-information/ScrapeConfigPage";
 
 function Index() {
   return (
@@ -9,6 +10,7 @@ function Index() {
       <Route path="/data-visualization" element={<DataVisualizationPage />} />
       <Route path="/extract-data" element={<ExtractDataPage />} />
       <Route path="/data-collection" element={<DataCollectionPage />} />
+      <Route path="/scrape-config" element={<ScrapeConfigPage />} />
     </Routes>
   );
 }
